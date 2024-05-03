40257e0112ea69113daa0f0f910bd516cd52afd9
Arth7 <arthnangar1@gmail.com>
2024-05-03 21:41:55 +0530

    Full Project Commit

---
+++ app/(root)/profile/page.tsx
@@ -0,0 +1,88 @@
+import Collection from "@/components/shared/Collection";
+import { Button } from "@/components/ui/button";
+import { getEventsByUser } from "@/lib/actions/event.actions";
+import { getOrdersByUser } from "@/lib/actions/order.action";
+import { IOrder } from "@/lib/database/models/order.model";
+import { SearchParamProps } from "@/types";
+import { auth } from "@clerk/nextjs";
+import { pages } from "next/dist/build/templates/app-page";
+import Link from "next/link";
+import React from "react";
+
+const ProfilePage = async ({searchParams}:SearchParamProps) => {
+	const { sessionClaims } = auth();
+
+	const userId = sessionClaims?.userId as string;
+
+	const ordersPage = Number(searchParams?.ordersPage) || 1
+
+	const eventsPage = Number(searchParams?.eventsPage) || 1
+
+	const organizedEvents = await getEventsByUser({ userId, page: eventsPage });
+	
+	const orders = await getOrdersByUser({ userId, page: ordersPage })
+	
+	const orderEvents = orders?.data.map((order: IOrder) => order.event) || []
+	
+	
+
+	return (
+		<>
+			{/* My Tickets  */}
+
+			<section className="bg-slate-300 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+				<div className="wrapper flex items-center justify-center sm:justify-between">
+					<h3 className="h3-bold text-center sm:text-left ">Booked Tickets</h3>
+					<Button
+						asChild
+						className="relative rounded-full scale-80 bg-gradient-to-r from-purple-800 via-blue-900 to-gray-900 text-white border-blue-500 transition-all duration-300 hover:bg-gray-800 hover:text-pink-500 hover:scale-125 button hidden sm:flex ">
+						<Link href="/#events">Explore More Events</Link>
+					</Button>
+				</div>
+			</section>
+
+			<section className="wrapper my-8">
+				<Collection
+					data={orderEvents}
+					emptyTitle="Tickets Await: You Haven't Purchased Yet!"
+					emptyStateSubtext="Explore thrilling events now - get your tickets!"
+					collectionType="My_Tickets"
+					limit={3}
+					page={ordersPage}
+					urlParamName="ordersPage"
+					totalPages={orders?.totalPages}
+				/>
+			</section>
+
+			{/* Event Organized */}
+
+			<section className="bg-slate-300 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+				<div className="wrapper flex items-center justify-center sm:justify-between">
+					<h3 className="h3-bold text-center sm:text-left ">
+						Events Organized
+					</h3>
+					<Button
+						asChild
+						className="relative rounded-full scale-80 bg-gradient-to-r from-purple-800 via-blue-900 to-gray-900 text-white border-blue-500 transition-all duration-300 hover:bg-gray-800 hover:text-pink-500 hover:scale-125 button hidden sm:flex ">
+						<Link href="/events/create">Explore More Events</Link>
+					</Button>
+				</div>
+			</section>
+
+			<section className="wrapper my-8">
+				<Collection
+					data={organizedEvents?.data}
+					emptyTitle="No Events Yet!"
+					emptyStateSubtext="Time to Create Some Events Now!"
+					collectionType="Event_Organized"
+					limit={3}
+					page={eventsPage}
+					urlParamName="eventsPage"
+					totalPages={organizedEvents?.totalPages}
+				/>
+			</section>
+		</>
+	);
+};
+
+export default ProfilePage;
---
+++ app/admin/users/page.tsx
@@ -0,0 +1,101 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import { getAllAdminUser, deleteAdminUser } from "@/lib/actions/user.action"; // Import deleteUser function
+import { Trash, Edit } from "lucide-react";
+
+interface IUser {
+	_id: string;
+	clerkId: string;
+	email: string;
+	username: string;
+	firstName: string;
+	lastName: string;
+	photo: string;
+}
+
+const AdminUser = () => {
+	const [users, setUsers] = useState<IUser[]>([]);
+
+	useEffect(() => {
+		// Fetch users data when component mounts
+		const fetchUsers = async () => {
+			try {
+				const fetchedUsers = await getAllAdminUser();
+				setUsers(fetchedUsers);
+			} catch (error) {
+				console.error("Error fetching users:", error);
+			}
+		};
+		fetchUsers();
+	}, []);
+
+	const handleDeleteUser = async (userId: string) => {
+		try {
+			await deleteAdminUser(userId);
+			setUsers((prevUsers) => prevUsers.filter((user) => user.clerkId !== userId));
+		} catch (error) {
+			console.error("Error deleting user:", error);
+		}
+	};
+
+	return (
+		<>
+			<section className=" bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+				<h3 className="wrapper h3-bold text-center sm:text-left ">Users</h3>
+			</section>
+			<section className="wrapper overflow-x-auto">
+				<table className="w-full border-collapse border-t">
+					<thead>
+						<tr className="p-medium-14 border-b text-grey-500">
+							<th className="min-w-[250px] py-3 text-left">User ID</th>
+							<th className="min-w-[200px] flex-1 py-3 pr-4 text-left">
+								Email
+							</th>
+							<th className="min-w-[150px] py-3 text-left">Username</th>
+							<th className="min-w-[100px] py-3 text-left">First Name</th>
+							<th className="min-w-[150px] py-3 text-left">Last Name</th>
+						</tr>
+					</thead>
+					<tbody>
+						{users && users.length === 0 ? (
+							<tr className="border-b">
+								<td colSpan={5} className="py-4 text-center text-gray-500">
+									No users found.
+								</td>
+							</tr>
+						) : (
+							<>
+								{users &&
+									users.map((row: IUser) => (
+										<tr
+											key={row._id}
+											className="p-regular-14 lg:p-regular-16 border-b "
+											style={{ boxSizing: "border-box" }}>
+											<td className="min-w-[250px] py-4 text-primary-500">
+												{row.clerkId}
+											</td>
+											<td className="min-w-[200px] flex-1 py-4 pr-4">
+												{row.email}
+											</td>
+											<td className="min-w-[150px] py-4">{row.username}</td>
+											<td className="min-w-[150px] py-4">{row.firstName}</td>
+											<td className="min-w-[150px] py-4">{row.lastName}</td>
+											<td className="min-w-[100px] py-4 text-right">
+												<div style={{ display: "flex" }}>
+													<button onClick={() => handleDeleteUser(row.clerkId)}>
+														<Trash size={24} />
+													</button>
+												</div>
+											</td>
+										</tr>
+									))}
+							</>
+						)}
+					</tbody>
+				</table>
+			</section>
+		</>
+	);
+};
+
+export default AdminUser;
